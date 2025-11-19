73c42829689e4ea93ab7ddc0283b60575859a30e
ataizhu <atai.zh@gmail.com>
2025-11-19 22:15:16 +0600

    Fix AppServiceProvider menu initialization for tenants - use file_exists instead of Storage

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -18,7 +18,9 @@ class AppServiceProvider extends ServiceProvider
         Paginator::useBootstrapThree();
         $this->loadJsonTranslationsFrom(resource_path('scripts/locales'));
 
-        if (\Storage::disk('local')->has('database_created') && Schema::hasTable('abilities')) {
+        $databaseCreatedFlag = storage_path('app/database_created');
+        
+        if (file_exists($databaseCreatedFlag) && Schema::hasTable('abilities')) {
             $this->addMenus();
         }
     }
