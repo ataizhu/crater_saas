244d4bc82a33c2aa6984a2397a7f7e47efb089f9
ataizhu <atai.zh@gmail.com>
2025-11-19 23:31:05 +0600

    fix: закомментировать IdeHelperServiceProvider для продакшн

--- config/app.php
+++ config/app.php
@@ -182,7 +182,7 @@ return [
         Crater\Providers\DropboxServiceProvider::class,
         Crater\Providers\ViewServiceProvider::class,
         App\Providers\TenancyServiceProvider::class,
-        Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class,
+        // Barryvdh\LaravelIdeHelper\IdeHelperServiceProvider::class, // Отключен для продакшн
     ],
 
     /*
